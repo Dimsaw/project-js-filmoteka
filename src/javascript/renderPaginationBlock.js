50fca9770739139be476bca16fa1d6c61d2cafc0
ArtemLytvynenkoa <chpart355@gmail.com>
2022-01-18 19:37:07 +0200

    adds some changes

--- src/javascript/renderPaginationBlock.js
+++ src/javascript/renderPaginationBlock.js
@@ -5,10 +5,9 @@ import Pagination from 'tui-pagination';
 
 const totalItemsParse = JSON.parse(localStorage.getItem("MoviesCollection"))
 const totalItems = totalItemsParse.total_pages;
-console.log(totalItems)
 
 const options = {
-  totalItems, 
+  totalItems,
   itemsPerPage: 1,
   visiblePages: 5,
   page: 1,
@@ -17,19 +16,18 @@ const options = {
   lastItemClassName: 'tui-last-child',
   template: {
     page: '<button class="tui-page-btn" >{{page}}</button>',
-    // '<a href="#" class="tui-page-btn">{{page}}</a>',
     currentPage: '<button class="tui-page-btn tui-is-selected" data-number="{{page}}" >{{page}}</button>',
     moveButton:
       '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</a>',
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip" >' +
-        '<span class="tui-ico-ellip">...</span>' +
+      '<span class="tui-ico-ellip">...</span>' +
       '</a>'
   }
 };
@@ -41,11 +39,9 @@ const paginationBox = document.querySelector('.tui-pagination');
 paginationBox.addEventListener('click', onButtonClick)
 
 function onButtonClick(event) {
-  Array.prototype.forEach.call(event.currentTarget.children, ({classList}) => {
+  Array.prototype.forEach.call(event.currentTarget.children, ({ classList }) => {
     if (classList.contains("tui-is-selected")) {
       const currentButton = document.querySelector('.tui-is-selected')
-      // const currentPage = JSON.stringify(currentButton.dataset.number);
-      // localStorage.setItem("currentPage", currentPage);
     }
   });
 }
