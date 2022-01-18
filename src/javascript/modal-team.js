3ec052ddb025a00ba597cb0444eb55f896ba9337
Anton Ptashynskyi <a.ptashynskyi@gmail.com>
2022-01-18 23:25:37 +0100

    Add backdrop

--- src/javascript/modal-team.js
+++ src/javascript/modal-team.js
@@ -1,16 +1,14 @@
 const modal = document.querySelector('#js-team-modal');
 const body = document.querySelector('body');
 
-console.log(modal);
+console.log(body);
+
+showTeamModal();
 
 function showTeamModal() {
-  //   modal.classList.toggle('hidden');
-
   if (!modal.classList.contains('hidden')) {
     body.style.overflow = 'hidden';
-    // body.style.overflow = 'auto';
   } else {
     body.style.overflow = 'auto';
-    // body.style.overflow = 'hidden';
   }
 }
