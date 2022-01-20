fa8620fcbf212b321b11b8c6135d0bdee4974fa7
Anton Ptashynskyi <a.ptashynskyi@gmail.com>
2022-01-20 22:37:46 +0100

    Remove back screen scroll

--- src/javascript/modal-team.js
+++ src/javascript/modal-team.js
@@ -2,25 +2,21 @@ const modal = document.querySelector('.js-team-modal');
 const body = document.querySelector('body');
 const closeButton = document.querySelector('.js-team-close');
 const students = document.querySelector('.js-students');
+const toTop = document.querySelector('#toTopBtn');
 
 students.addEventListener('click', handleStudents);
-
-showTeamModal();
-
-function showTeamModal() {
-  if (!modal.classList.contains('hidden')) {
-    body.style.overflow = 'hidden';
-  } else {
-    body.style.overflow = 'auto';
-  }
-}
 
 function handleStudents(e) {
   e.preventDefault();
   modal.classList.remove('hidden');
+  body.style.overflow = 'hidden';
+  toTop.classList.add('js-hidden');
+
   closeButton.addEventListener('click', onCloseBtnClick);
 }
 
 function onCloseBtnClick() {
   modal.classList.add('hidden');
+  body.style.overflow = 'auto';
+  toTop.classList.remove('js-hidden');
 }
