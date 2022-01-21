71a15b6e9a71861ea4e3aa1a8f9e279f65e915ce
SNagirniy <86325813+SNagirniy@users.noreply.github.com>
2022-01-21 20:49:02 +0200

    deleted comments

--- src/javascript/fetchMovies.js
+++ src/javascript/fetchMovies.js
@@ -10,17 +10,17 @@ const movieApiService = new apiService();
 searchForm.addEventListener('submit', onSubmitForm);
 
 
-////Отримує дані при завантаженні сторінки\\\\
+
 try {
   fetchMovies();
   const genres = movieApiService.fetchGenres();
-  genres.then(genre => localStorage.setItem("genres", JSON.stringify(genre)) )// Додає список жанрів при першому завантаженні до localstorage
+  genres.then(genre => localStorage.setItem("genres", JSON.stringify(genre)) )
 } catch (error) {
   console.log(error);
 }
 
 
-////Пошук фільмів при сабміті форми\\\\
+
 function onSubmitForm(e) {
   e.preventDefault();
   movieApiService.searchQuery = e.currentTarget.elements.query.value.trim();
@@ -64,16 +64,12 @@ async function fetchMovies(page = 1) {
 
 
 
-
-
-//// Додає масив фільмів до LocalStorage\\\\
-
 function addMoviesCollectionToLocalStorage(moviesArray) { 
   localStorage.removeItem("MoviesCollection");
   localStorage.setItem("MoviesCollection", JSON.stringify(moviesArray));
 };
 
-//// Виводить error message\\\\
+
 function addErrorMessage() { 
   const errorIsVisible = document.querySelector('.error-message is_visible');
   if (errorIsVisible) { return }
@@ -82,7 +78,7 @@ function addErrorMessage() {
   errorMessage.classList.toggle('is_hidden')
   
  };
-//// Очищає поле вводу та прибирає error message\\\\
+
 function removeErrorMessage() { 
   errorMessage.classList.toggle('is_hidden')
   errorMessage.classList.toggle('is_visible')
