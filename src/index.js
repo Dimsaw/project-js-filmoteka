02ce8f4d977258a71ad340a50be52503255493b9
Anton Ptashynskyi <86067969+AntonPtashynskyi@users.noreply.github.com>
2022-01-22 18:19:33 +0100

    remove coments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './sass/main.scss';
 
-// import './javascript/README'; // Так подключаем модули JS файлов.
-// import './javascript/fetch';
 import './javascript/renderPaginationBlock';
 import './javascript/library';
 import './javascript/fetchMovies';
