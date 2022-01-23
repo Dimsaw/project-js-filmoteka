439fd301e74b8201db22b30dddc67d31d5e2d9f3
Dima Savkov <dimsaw85@gmail.com>
2022-01-23 17:09:40 +0200

    finish button modal library

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -5,12 +5,11 @@ const modal = document.querySelector('[data-modal]');
 const filmsContainer = document.querySelector('.films__list');
 const modalUI = document.querySelector('.modalUI');
 
-
 filmsContainer.addEventListener('click', onFilmClick);
 modalCloseBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-    modal.classList.toggle('is-hidden');
+  modal.classList.toggle('is-hidden');
 }
 
 function onFilmClick(e) {
@@ -18,32 +17,33 @@ function onFilmClick(e) {
     return;
   } else {
     toggleModal();
-    modalUI.innerHTML = "";
+    modalUI.innerHTML = '';
     showFilmInfo(e);
   }
 }
 
-function showFilmInfo(e){
-    const currentFilms = JSON.parse(localStorage.getItem("MoviesCollection")).results;
+function showFilmInfo(e) {
+  const currentFilms = JSON.parse(localStorage.getItem('MoviesCollection')).results;
 
-    
-    
-    currentFilms.map(item => {
+  currentFilms.map(item => {
+    // console.log(genresTextArray(item.genre_ids));
 
-        
-        // console.log(genresTextArray(item.genre_ids));
-
-        if(e.target.id === String(item.id)){
-            
-            modalUI.insertAdjacentHTML("afterbegin",
-            `<img src="https://image.tmdb.org/t/p/w500${item.poster_path}" width="100%" height="100%" alt="" class="film-preview-img" />
+    if (e.target.id === String(item.id)) {
+      localStorage.setItem('currentFilm', JSON.stringify(item));
+      modalUI.insertAdjacentHTML(
+        'afterbegin',
+        `<img src="https://image.tmdb.org/t/p/w500${
+          item.poster_path
+        }" width="100%" height="100%" alt="" class="film-preview-img" />
 
             <div>
               <h1 class="h1">${item.title}</h1>
               <table class="table-info">
                 <tr>
                   <td class="modal-info">Vote / Votes</td>
-                  <td><span class="vote-modal">${item.vote_average}</span> / <span class="votes-modal">${item.vote_count}</span></td>
+                  <td><span class="vote-modal">${
+                    item.vote_average
+                  }</span> / <span class="votes-modal">${item.vote_count}</span></td>
                 </tr>
                 <tr>
                   <td class="modal-info">Popularity</td>
@@ -63,15 +63,56 @@ function showFilmInfo(e){
               <p class="modal-overview">${item.overview}</p>
               
               <ul class="buttons-list">
-                <li><button type="submit" class="btn-modal">ADD TO WATCHED</button></li>
+                <li><button type="submit" class="btn-modal modal__watched"></button></li>
                 <li><button type="submit" class="btn-modal">ADD TO QUEUE</button></li>
               </ul>
-            </div>`);
-        }
-        
-    });
+            </div>`,
+      );
+    }
+  });
 
-    
+  const modalWatchedButton = document.querySelector('.modal__watched');
+
+  const currentFilm = JSON.parse(localStorage.getItem('currentFilm'));
+
+  const watchedFilms = JSON.parse(localStorage.getItem('watched')) || [];
+
+  localStorage.setItem('watched', JSON.stringify(watchedFilms));
+
+  if (watchedFilms.find(watchedFilm => watchedFilm.id === currentFilm.id)) {
+    document.querySelector('.modal__watched').innerText = 'DELETE FROM WATCHED';
+  } else {
+    document.querySelector('.modal__watched').innerText = 'ADD TO WATCHED';
+  }
+
+  modalWatchedButton.addEventListener('click', onClickWatchedButton);
 }
 
-// console.log()
+function onClickWatchedButton(e) {
+  console.log('click Watched Button');
+  toggleText();
+
+  const currentFilm = JSON.parse(localStorage.getItem('currentFilm'));
+  const watchedFilms = JSON.parse(localStorage.getItem('watched')) || [];
+
+  if (watchedFilms.find(watchedFilm => watchedFilm.id === currentFilm.id)) {
+    localStorage.setItem(
+      'watched',
+      JSON.stringify(watchedFilms.filter(watchedFilm => watchedFilm.id !== currentFilm.id)),
+    );
+    document.querySelector('.modal__watched').innerText == 'ADD TO WATCHED';
+    return;
+  }
+  document.querySelector('.modal__watched').innerText = 'DELETE FROM WATCHED';
+  watchedFilms.push(currentFilm);
+
+  localStorage.setItem('watched', JSON.stringify(watchedFilms));
+}
+
+function toggleText(e) {
+  if (document.querySelector('.modal__watched').innerText == 'ADD TO WATCHED') {
+    document.querySelector('.modal__watched').innerText = 'Delete from watched';
+  } else {
+    document.querySelector('.modal__watched').innerText = 'ADD TO WATCHED';
+  }
+}
