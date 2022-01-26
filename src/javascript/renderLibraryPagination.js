b64b675500231f56d5cf01f304694ad5823b54ff
ArtemLytvynenkoa <chpart355@gmail.com>
2022-01-26 23:43:38 +0200

    adds changes

--- src/javascript/renderLibraryPagination.js
+++ src/javascript/renderLibraryPagination.js
@@ -30,79 +30,16 @@ const libraryPaginationOptions = {
 
 const libraryPagination = new Pagination('library-pagination', libraryPaginationOptions);
 
-window.addEventListener("resize", function() {
-  if (window.matchMedia("(max-width: 768px - 1)").matches) {
-    console.log(libraryPaginationOptions.itemsPerPage);
-    libraryPagination.setItemsPerPage(4);
-    console.log(libraryPaginationOptions.itemsPerPage);
-    return
-  }
-
-  if (window.matchMedia("(max-width: 1024px - 1)").matches) {
-    console.log(libraryPaginationOptions.itemsPerPage);
-    libraryPagination.setItemsPerPage(8);
-    console.log(libraryPaginationOptions.itemsPerPage);
-    return
-  }
-
-  // if (window.matchMedia("(min-width: 1024)").matches) {
-    console.log(libraryPaginationOptions.itemsPerPage);
-    libraryPagination.setItemsPerPage(9);
-    console.log(libraryPaginationOptions.itemsPerPage);
-  //   return
-  // }
-
-});
-
 function searchMoviesForLibrary(page, library) {
-  window.addEventListener("resize", function() {
-    if (window.matchMedia("max-width: 767px").matches) {
-      let movies = [];
-      let indexElement = 4 * (page-1);
-      library.filter((element, index) => {
-        if ((index + 1 <= 4 * page) && (index + 1 > indexElement)) {
-          movies.push(element);
-        }
-      })
-
-      renderLibraryMarkup(movies);
-      return
+  let movies = [];
+  let indexElement = 9 * (page-1);
+  library.filter((element, index) => {
+    if ((index + 1 <= 9 * page) && (index + 1 > indexElement)) {
+      movies.push(element);
     }
+  })
 
-    if (window.matchMedia("(max-width: 1023px)").matches) {
-      let movies = [];
-      let indexElement = 8 * (page-1);
-      library.filter((element, index) => {
-        if ((index + 1 <= 8 * page) && (index + 1 > indexElement)) {
-          movies.push(element);
-        }
-      })
-
-      renderLibraryMarkup(movies);
-      return
-    }
-
-    // if (window.matchMedia("(min-width: 1024)").matches) {
-      let movies = [];
-      let indexElement = 9 * (page-1);
-      library.filter((element, index) => {
-        if ((index + 1 <= 9 * page) && (index + 1 > indexElement)) {
-          movies.push(element);
-        }
-      })
-      renderLibraryMarkup(movies);
-      // return
-    // }
-});
-  // let movies = [];
-  // let indexElement = 9 * (page-1);
-  // library.filter((element, index) => {
-  //   if ((index + 1 <= 9 * page) && (index + 1 > indexElement)) {
-  //     movies.push(element);
-  //   }
-  // })
-
-  // renderLibraryMarkup(movies);
+  renderLibraryMarkup(movies);
 }
 
 function returnQueue() {
